76a9ff7366916319f43f8460281294d07d0eddf5
vikram-mor <revenantmor@gmail.com>
2021-11-28 22:48:18 +0530

    Updated

--- src/components/app.js
+++ src/components/app.js
@@ -9,11 +9,27 @@ class App extends React.Component{
        
       }
 
-          
+authfunc(){
+  const authData = { data: "Auth on my site" };
+  if(WavesKeeper){
+  WavesKeeper.auth(authData)
+      .then(auth => {
+          console.log(auth); //displaying the result on the console
+          /*...processing data */
+      }).catch(error => {
+          console.error(error); // displaying the result on the console
+          /*...processing errors */
+      })
+  }
+  else{
+    alert("Need Waveskeeper to auth");
+  }
+
+}  
       render(){
         return(
           <div className="container">
-              <input className="btn btn-primary" type="submit" value="Auth" onClick={() =>{alert("Alert");}}></input>
+              <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authfunc}></input>
           </div>
         )
       }
